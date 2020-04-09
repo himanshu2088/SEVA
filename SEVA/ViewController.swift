3a37e98812dfa10d3a0e3527590910bd7a538fba
Himanshu Joshi <himanshujoshi2088@gmail.com>
2020-04-09 19:01:09 +0530

    Initial Commit

---
+++ SEVA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SEVA
+//
+//  Created by Himanshu Joshi on 09/04/20.
+//  Copyright © 2020 Himanshu Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
